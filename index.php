dd0c9f7364d809987911ab8ad7a40e58b26f64d9
Grant <grantlauren@wireless-10-145-85-84.public.utexas.edu>
2016-08-30 13:35:09 -0500

    first commit, adding all files

---
+++ index.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>MakerStudios</title>
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,800,700,600,300" type="text/css" media="all" />
+	 <link rel="stylesheet" href="/makerstudios-dev/css/main.css" type="text/css" media="all" />
+	 <link rel="stylesheet" href="/makerstudios-dev/css/tablet.css" type="text/css" media="all" />
+	 <link rel="stylesheet" href="/makerstudios-dev/css/mobile.css" type="text/css" media="all" />
+
+	 <script   src="https://code.jquery.com/jquery-3.0.0.min.js"   integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0="   crossorigin="anonymous"></script>
+	 <script   src="https://code.jquery.com/jquery-migrate-3.0.0.min.js"   integrity="sha256-JklDYODbg0X+8sPiKkcFURb5z7RvlNMIaE3RA2z97vw="   crossorigin="anonymous"></script>
+
+	  <script src="/makerstudios-dev/js/makerstudios.js" type="text/javascript"></script>
+
+</head>
+<body>
+<div id="wrapper">
+
+	<div id="mast">
+		<!-- <div class="moduletable-mobilenav">
+			<h3>MENU &#9776</h3>
+			<ul class="nav menu-mobile">
+				<li class="item parent"><a href="/about-us">About Us</a></li>
+				<li class="item parent"><a href="/academics">Academics</a></li>
+				<li class="item parent"><a href="/research">Research</a></li>
+				<li class="item parent"><a href="/people">People</a></li>
+				<li class="item parent"><a href="/events">Events</a></li>
+				<li class="item parent"><a href="/news">News</a></li>
+			</ul>
+		</div> -->
+
+		<div class="custom-cselogo">
+			<a href="http://www.engr.utexas.edu"><span>Cockrell School of Engineering</span></a>
+		</div>
+
+		<div class="custom-utlogo">
+			<a href="http://www.utexas.edu"><span>The University of Texas at Austin</span></a>
+		</div>
+	</div>	
+
+	<div id="submast">
+		<div class="custom-makerstudioslogo">
+			<a href="http://makerstudios.engr.utexas.edu"><img src="images/logo.jpg"><span>MakerStudios</span></a>
+		</div>
+
+		<div class="search">
+			<div class="search-box">
+				<p>Search</p>
+			</div>
+		</div>
+
+		<div class="moduletable-mobilenav">
+			<h3><span>MENU</span> &#9776</h3>
+			<ul class="nav menu-mobile">
+				<li class="item parent deeper"><a href="/about">About the Studio</a></li>
+				<li class="item parent deeper"><a href="/equipment">Equipment</a></li>
+				<li class="item parent deeper"><a href="/locations">Facilities</a></li>
+				<li class="item parent deeper"><a href="/contact">Contact</a></li>
+				<li id="search" class="item parent deeper"><div class="search-box"><a href="/search">Search</a></div></li>
+			</ul>
+		</div>
+	</div>	
+
+	<div class="moduletable-mainnav">
+		<ul class="nav menu">
+			<li class="item parent deeper"><a href="/about">About the Studio</a></li>
+				<li class="item parent deeper"><a href="/equipment">Equipment</a></li>
+				<li class="item parent deeper"><a href="/locations">Facilities</a></li>
+				<li class="item parent deeper"><a href="/contact">Contact</a></li>
+		</ul>
+	</div>
+
+	<div class="custom-homehero">
+		<div class="herotext">
+			<h1><a href="">Longhorn MakerStudios is a makerspace where engineering students can come and make things.<span> Learn more  </span></a></h1>
+		</div>
+	</div>
+
+	<div id="position-2">
+
+		<div class="flex-float">
+		
+		<div class="custom-flex two-b">
+			<p class="category">MakerStudio 1</p>
+			<h3>ETC 1.222</h3>
+			<p class="hours">Monday - Friday: 8am - 10pm<br />Saturday - Sunday: 12 - 7pm</p>
+			<p class="button-sm-blue-inv"><a href="">Facility Details</a></p>
+			<!-- <p>3D printers and more. <span>Details </span></p> -->
+
+			<!-- <p>Resources include 3D printers, laser cutters, 2D routers for manufacturing printed circuit boards,  testing and electronics equipment, hand tools and more.</p> -->	
+		</div>
+		
+		<div class="custom-flex three-b">
+			<p class="category">MakerStudio 2</p>
+			<h3>ETC 6.206</h3>
+			<p class="hours">Monday - Friday: 8am - 4pm<br />Closed on weekends.</p>
+			<p class="button-sm-blue-inv"><a href="">Facility Details</a></p>
+			<!-- <p>Complete wood shop and more. <span>Details </span></p> -->
+			<!-- <p>Resources include a complete wood shop with a table saw, band saws, drill presses, radial arm saws, routers, CNC X Carve and an assortment of hand tools.
+			</p> -->
+		</div>
+		<div class="custom-flex four-b">
+			<p class="category">MakerStudio 3</p>
+			<h3>ETC 1.208 / 1.210</h3>
+			<p class="hours">Monday - Friday: 8am - 4pm<br />Closed on weekends.</p>
+			<p class="button-sm-blue-inv"><a href="">Facility Details</a></p>
+			<!-- <p>Metal working equipment and more. <span>Details </span></p> -->
+			<!-- <p class="category-desc">Resources include a 3D CNC Milling Machine, a plasma cutter and other metal working/machining equipment.</p> -->
+		</div>
+		</div>
+
+		<div class="custom-flex one-b">
+			<div class="flex-image">
+				<a href="/"><img src="/makerstudios-dev/images/eerc-outside.jpg"></a>
+			</div>	
+			<p class="category">News</p>
+			<h3>NEW LOCATION OPENING SOON</h3>	
+			<p class="category-desc">Longhorn MakerStudios will open in our new location in Fall 2017. Join us at the new Engineering Education Research Center.</p>
+			<p class="button-sm-blue-inv"><a href="">Read More</a></p>
+		</div>
+
+		<div class="custom-flex four">
+			<div class="custom-video">
+				<iframe width="640" height="480" src="https://www.youtube.com/embed/Y1HONETZZVA?rel=0&amp;showinfo=0" frameborder="0"></iframe>
+			</div>
+			<p class="category-desc"><span>EQUIPMENT<br /><br /></span>We specialize in easy-to use equipment such as 3D printers and laser cutters as well as a full suite of testing and electronics equipment, hand tools, and more. Watch a video that shows what you can do with our equipment, then <a href="/resources">view the full list</a>.<br /><br />
+			All Engineering undergraduate and graduate students are welcome. We do not charge to use the equipment.</p>
+		</div>
+	</div>	
+	
+	<div class="moduletable-flexcontrols">
+		<div class="custom-flexcontrols">
+			<ul class="circlecontrol">
+				<li id="circleone"><a href="#null"></a></li>
+				<li><a href="#two"></a></li>
+				<li><a href="#three"></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="footer">
+	
+		<div class="custom-copyright">
+			<p><a href="">MakerStudios</a><br />
+			204 E. Dean Keeton Street, Stop C2200<br />
+			Austin, Texas 78712-1591</p>
+			<p>&#169 2016 MakerStudios</p>
+			<div class="custom-utlinks-mobile">
+				<p><a href="http://www.engr.utexas.edu">Cockrell School of Engineering</a><br />
+				<a href="http://www.utexas.edu">The University of Texas at Austin</a></p>
+			</div>
+		</div>
+	
+		<div class="custom-contact">
+			<h2>Contact</h2>
+			<p>Steve Ferraro<br />
+			MakerStudios Manager<br />
+			<a href="mailto:s.ferraro@me.utexas.edu">s.ferraro@me.utexas.edu</a></p>
+			<p>Alejandro Marez<br />
+			Wood Shop Manager<br />
+			<a href="mailto:marez@austin.utexas.edu">marez@austin.utexas.edu</a></p>
+		</div>
+		
+		<div class="custom-resources">
+			<span class="switch"><a href="#resources">University Information &#9662</a></span>
+			<h2>Resources</h2>
+			<ul id="resources" class="expand">
+				<li><a href="http://directory.utexas.edu/">UT Directory</a></li>
+				<li><a href="https://utdirect.utexas.edu/utdirect/">UT Direct</a></li>
+				<li><a href="http://www.utexas.edu/web-privacy-policy">Privacy Policy</a></li>
+				<li><a href="http://www.utexas.edu/web-accessibility-policy">Accessibility</a></li>
+				<li><a href="http://www.utexas.edu/web/guidelines/browsers.html">Recommended Browsers</a></li>
+			</ul>
+		</div>
+		
+		<div class="custom-social">
+			<h2>Connect with the Cockrell School</h2>
+			<li><a href=""><img src="/makerstudios-dev/images/youtube-32-grey.png"></a></li>
+			<li><a href=""><img src="/makerstudios-dev/images/instagram-new-32-grey.jpg"></a></li>
+			<li><a href=""><img src="/makerstudios-dev/images/twitter-32-grey.png"></a></li>
+			<li><a href=""><img src="/makerstudios-dev/images/facebook-32-grey.png"></a></li><br />
+				<div class="custom-utlinks">
+				<p><a href="http://www.engr.utexas.edu">Cockrell School of Engineering</a></p>
+				<p><a href="http://utexas.edu">The University of Texas at Austin</a></p>
+				</div>
+		</div>
+		
+	</div>
+
+</div>
+
+</body>
+</html>
---
+++ javascript/makerstudios.js
@@ -0,0 +1,118 @@
+(function($) {
+
+	$(function(){
+		$('.custom-makerstudioslogo, .custom-cselogo, .custom-utlogo').click(function(){
+			window.location=$(this).find("a").attr("href");
+			return false;
+		});
+	});
+
+	$(function() {
+		$(".moduletable-mainnav > .menu > li.deeper, #left > .menu-subnav > li.deeper").mouseenter(function(){
+			var navclass = $(this).attr("class");
+		  $(this).addClass("showme");
+		}).mouseleave(function(){
+			var navclass = $(this).attr("class");
+		  $(this).removeClass("showme");
+		});
+	});
+
+	$(function(){
+		$( '.menuhead h3' ).click(function(){
+			$('#left .menu-subnav').slideToggle();
+		});		
+	});
+
+	$(function(){
+		$('.moduletable-mobilenav').click(function(){
+			$('#submast .menu-mobile').slideToggle().toggleClass('expand');
+		})
+	});
+
+	$(function(){
+		$( '.moduletable-mobilenav h3' ).click(function(){
+			$('.moduletable-mobilenav h3').css({'z-index': '999'});
+			var docHeight = $(document).height();
+			$("body").append("<div id='overlay'></div>");
+			$('#mast .menu-mobile').slideToggle();
+			$("#overlay").height(docHeight); 
+		   	$('#overlay').toggleClass('show');
+		});		
+	});
+
+	
+
+	$(function(){
+		$( '.moduletable-flexcontrols p a.next, .moduletable-flexcontrols p a.prev' ).click(function(event){
+			var n = $(this).attr('href').substring(1);
+			if (n == 'null') {
+				return false;
+			} else {
+				$('.'+ n).css({'display':'block'});
+				$('.moduletable-flex:not(.'+ n +')').css({'display':'none'});
+				switch (n) {
+					case "one" :
+						$('.moduletable-flexcontrols p a.next').attr('href', '#two');
+						$('.moduletable-flexcontrols p a.prev').attr('href','#null').addClass('disabled');
+						break;
+					case "two" :
+						$('.moduletable-flexcontrols p a.next').attr('href', '#three').removeClass('disabled');
+						$('.moduletable-flexcontrols p a.prev').attr('href','#one').removeClass('disabled');
+						break;
+					case "three" :
+						$('.moduletable-flexcontrols p a.next').attr('href','#null').addClass('disabled');
+						$('.moduletable-flexcontrols p a.prev').attr('href','#two');
+						break;
+					}
+			}
+		});		
+	});
+
+	$(function() {
+
+		$(".switch a").click(function(e) { 
+
+			var link = $(this).attr("href");
+			
+			$(link).slideToggle();
+			$(this).toggleClass('active');
+			
+			e.preventDefault(); 
+
+		});
+	});
+
+	$(function() {
+		// Find all videos
+		var $allVideos = $("iframe"),
+
+    	// The element that is fluid width
+    	$fluidEl = $(".custom-video, div.item-page, div.blog, div.blog-undergrad, div.blog-news, div.item-pagealumprofiles, .item-page-undergrad, div.item-page-news, div.undergradprograms.undergradprograms");
+
+		// Figure out and save aspect ratio for each video
+		$allVideos.each(function() {
+  			$(this)
+    		.data('aspectRatio', this.height / this.width)
+
+    		// and remove the hard coded width/height
+    		.removeAttr('height')
+    		.removeAttr('width');
+		});
+
+		// When the window is resized
+		$(window).resize(function() {
+  			var newWidth = $fluidEl.width();
+
+  		// Resize all videos according to their own aspect ratio
+	  		$allVideos.each(function() {
+	    		var $el = $(this);
+	    		$el
+	      			.width(newWidth)
+	      			.height(newWidth * $el.data('aspectRatio'));
+			});
+
+		// Kick off one resize to fix all videos on page load
+		}).resize();
+	});
+
+})(jQuery);
